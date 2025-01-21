1f0dd226bd342c764e51be10107e35461e110357
RaelViana <catedralviana@hotmail.com>
2025-01-21 16:50:18 -0300

    realizando agrupamento para ticket médio

--- meu_arquivo.py
+++ meu_arquivo.py
@@ -9,11 +9,15 @@ pd.set_option('display.max_columns', None) ### Mostrar o máximo de colunas
 # Faturamento por loja
 faturamento = tabela_vendas[['ID Loja','Valor Final']].groupby('ID Loja').sum()
 print(faturamento)
+print('-' * 50)
 
 # Quantidade de produtos vendidos por loja
 quantidade = tabela_vendas[['ID Loja','Quantidade']].groupby('ID Loja').sum()
 print(quantidade)
+print('-' * 50)
 
 # Ticket médio por produto em cada loja
+ticket_medio = (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()
+print(ticket_medio)
 
 # Enviar um email com o relatório
